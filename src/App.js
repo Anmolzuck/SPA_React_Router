cd34d9d3627f31197a6ae4f490acf39249aab212
Anmol Zakie <anmolzuck17@gmail.com>
2021-09-27 13:46:20 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Product";
+import ProductDetails from "./pages/ProductDetails";
+import MainHeader from "./components/MainHeader";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
